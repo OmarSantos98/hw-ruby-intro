7c8ae17072cf928cfe2a88a1d40caf8b7953df83
Omar Santos <omars98@tamu.edu>
2019-01-02 11:03:21 +0800

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,13 +3,9 @@
 
 
 
-def price_as_string
-return "$#{"%.2f%",@price}"
-
-end
 # Part 1
 
-def sum (arr)
+def sum arr
   # YOUR CODE HERE
  sumTotal=0
 
@@ -22,83 +18,55 @@ end
   return sumTotal
 end
 
-def max_2_sum (arr)
-  # YOUR CODE HERE
-  if arr.length==0
-return 0
-end
-
-if arr.length==1
-return arr[0]
-end
-
-biggest=0
-biggest2=0
-
-for i in arr do
-
-if i>biggest
-biggest=i
-end
-end
-
-for i in 0..arr.length do
-
-if arr[i]==biggest
-arr.delete_at(i)
-break
-end
-end
-
-for i in arr do
-
-if i>biggest2
-biggest2=i
+def max_2_sum arr
+  num = 0
+  if arr.length > 0
+    arr.sort!
+    num += arr.pop
+    if arr.length > 1
+      num += arr.pop
+    end
+  end
+  return num
 end
 
 
+
+def sum_to_n? arr, n
+  arr.sort!
+  
+  return false if arr.empty?
+  return false if arr.length == 1
+  
+  num1 = arr[0]
+  num2 = arr[arr.length - 1]
+
+#pseudocode for algorithm from internet  
+  while num1 < num2  do
+    if num1 + num2 == n
+      return true
+    elsif num1 + num2 < n
+      num1 += 1
+    else
+      num2 -= 1
+    end
+  end
+  
+  return false
 end
-
-return biggest+biggest2
-end
-
-def sum_to_n? (arr, n)
-  # YOUR CODE HERE
-   # YOUR CODE HERE
-for i in 0..arr.length do
-
-
-for j in 1..arr.length do
-
-if i+j==n
-return true
-else
-return false
-
-end
-end
-
-end
-end
-
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
-   "Hello, #{name}"
+ return "Hello, " + name
 end
 
-def starts_with_consonant? (s)
+def starts_with_consonant? s
   # YOUR CODE HERE
-  if  s[0]=='A' || s[0]=='E' ||  s[0]=='I' || s[0]=='O' ||  s[0]=='U' ||  s[0]=='a' || s[0]=='e' ||  s[0]=='i' || s[0]=='o' ||  s[0]=='u'
- return false
-
- else
- return true
- end
+ s[0] =~ /[b-df-hj-np-tv-z]/i
 end
 
-def binary_multiple_of_4? (s)
+def binary_multiple_of_4? s
   # YOUR CODE HERE
   s2 = s
 if s2.delete("01") != ""
@@ -114,24 +82,16 @@ end
 # Part 3
 
 class BookInStock
-
-attr_accessor:isbn
-attr_accessor:price
-def initialize (isbn,price)
-  if isbn.length== 0
-    raise ArgumentError, 'isbn is empty!'
+  attr_accessor :isbn, :price
+  def initialize isbn, price
+    if isbn.length == 0 or price <= 0
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
   end
-  if price<=0 
-    raise ArgumentError, 'price is negative or zero!'
+  
+  def price_as_string
+    return "$%.2f" % @price
   end
-  @isbn=isbn
-  @price=price
-  
-  
-end
-
-def price_as_string
-  return "$%.2f"%price   
-end
-
 end
